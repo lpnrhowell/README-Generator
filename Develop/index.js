c227f8ddd6d0da74de4a10db12c3c07d87f937a6
Steel <lpnrhowell@gmail.com>
2021-05-03 18:28:29 -0400

    modified index.js, README.md, markDown, and added package-lock.json

--- Develop/index.js
+++ Develop/index.js
@@ -1,66 +1,126 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
-const inquirer=require("inquirer");
+function init() {
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generate = require("./utils/markDown");
+const path = require("path");
+
+
+//  array of questions for user
+// const questions = [
+
+// const inquirer=require("inquirer");
+// const { Console } = require("console");
 inquirer.prompt([
     {
         type:"input",
-        name:"firstName",
-        message:"Enter your First Name"
+        message:"What is the title of your project",
+        name:"Title",
     },
     {
         type:"input",
-        name:"lastName",
-        message:"Enter your Last Name"
+        message:"Please write a discription of your project?",
+        name:"Discription"
+    },
+    { 
+        type:"input",
+        message:"Installation instructions",
+        name:"Install"
+    },
+    { 
+        type:"input",
+        message:"Explain how to use the app",
+        name:"Usage"
+    },
+    { 
+        type:"List",
+        message:"Which License are you using?",
+        name:"License",
+        choices: [
+            "MIT LIcense",
+            "GVL GPL License",
+            "Apache License",
+            "No License",
+        ]
+    },
+    { 
+        type:"input",
+        message:"Are there any Contributions?",
+        name:"Contributions"
+    },
+    { 
+        type:"input",
+        message:"How to test the app",
+        name:"Test"
+    },
+    { 
+        type:"input",
+        message:"Any Questions?",
+        name:"Questions"
     },
     { type:"input",
-        from:"Where are you from",
-        message:"Enter Where are you from",
-        name:"from"
+        message:"What is your github UserName",
+        name:"Github"
     },
-    { type:"input",
-        hobby:"What is your favorite hobby",
-        message:"Enter your favorite hobby",
-        name:"hobby"
-    },
-    { type:"input",
-        food:"What is your favorite food",
-        message:"Enter your favorite food",
-        name:"food"
-    },
-    { type:"input",
-        GitHub:"What is your GitHub Username",
-        message:"Enter your github Username",
-        name:"github"
-    },
-    { type:"input",
-        LinkedIn:"What is your LinkedIn URL",
-        message:"Enter your LinkedIn URL",
-        name:"linkedIn"
-    },
-]).then(result=>{
+    { 
+        type:"input",
+        message:"What is your email?",
+        name:"Email"
+    }
 
-const html=`<!DOCTYPE html>
-<html>
-<head>
-<title>Portfolio</title>
-</head>
-<body>
-Hi, my name is ${result.firstName}
-</body>
-</html>`;  
+])
+  .then((response) => {
+    return fs.writeFileSync(path.join (process.cwd(), "README.md"), generate(response));
+  });
+}
 
-console.log(result.firstName,result.lastName,result.from,result.hobby,result.food,result.github,result.linkedIn);
+init();
+// .then(result=> {
+// console.log
+// (result.Title,result.Discription,result.Install,result.Usage,result.License,result.Contributions,result.Test, result.Questions, result.Github, result. Email);
 
-});
+// const html=`<!DOCTYPE html>
+// <html>
+// <head>
+// <title>Portfolio</title>
+// </head>
+// <body>
+// Hi, my name is ${result.firstName}
+// </body>
+// </html>`;  
 
+// console.log(html)
+// }
+//  Function to write File
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data, (err) => {
+//         if (err) {
+//           throw err;
+//         }
+//         console.log("ReadMe was created");
+//       });
+//     }
+
+// // function to initialize program
+// function init() {
+//     inquirer.prompt(questions).then((answers) => {
+
+//         const response = markDown(answers);
+//         console.log(answers);
+
+//         writeToFile("README.md" , response);
+
+//     });   
+
+// }
---
+++ Develop/utils/markDown.js
@@ -0,0 +1,76 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+// function markDown(data) {
+//   return `# ${response.title}
+// function to generate markdown for README
+function licenseBadge(response) {
+  const licenseType = response.license[0];
+  let licenseString = " "
+  if (licenseType === "MIT") {
+    licenseString = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  };
+  if (licenseType === "GNU General Public License 2.0") {
+    licenseString = `![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)`
+  };
+  if (licenseType === "Apache License 2.0") {
+     licenseString = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
+    };
+  if (licenseType === "GNU General Public License 3.0") {
+      licenseString = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+    };
+  return licenseString
+  };
+
+function markDown(response) {
+  return `
+
+  # ${response.title}
+## Table of Contents
+# [Discription](#discription)
+# [Installation](#installation)
+# [#Usage](#usage)
+# [License](#license)
+# [Contributors](#contributors)
+# [Test](#test)
+# [Questions](#questions)
+
+## Distribution
+${response.Distribution}
+
+## Installation
+${response.Install}
+
+## Usage
+${response.Usage}
+
+## License
+${response.License}
+
+## Contributors
+${response.Contributors}
+
+## Test
+${response.Test}
+
+
+## Questions
+If you want to contact me:
+
+Github:[${response.Github}](https://github.com/lpnrhowell)
+
+Email:[${response.Email}](lpnrhowell@gmail.com)
+
+`;}
+
+module.exports = markDown
